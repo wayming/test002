c3d7882d2e692500c1109eab27957ca0af81d686
wayming zhang <wayming.z@gmail.com>
2017-04-28 14:54:46 +1000

    configuration

--- src/App.js
+++ src/App.js
@@ -13,10 +13,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {world}
+        <div>
+          {world}
+        </div>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,12 @@
  webpack.config.js
 */  
 const path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  filename: 'index.html',
-  inject: 'body'
-})
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve('build'),
+        publicPath: "/public/",
         filename: 'bundle.js'
     },
     module: {
@@ -21,7 +16,5 @@ module.exports = {
             { test: /\.css$/, loader: "style-loader!css-loader", exclude: /node_modules/},
             { test: /\.svg$/, loader: "url-loader", exclude: /node_modules/}
         ]
-    },
-    plugins: [HtmlWebpackPluginConfig]
-
+    }
 }
